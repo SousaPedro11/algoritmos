f90b881697ecc121b37218d7305a502a44d1d7d2
Pedro Sousa <ppls2106@gmail.com>
2021-04-19 16:30:55 -0300

    feat(matriz): adicionando implementacao dos quadrantes

--- python_implementation/matriz/quadrada.py
+++ python_implementation/matriz/quadrada.py
@@ -1,38 +1,72 @@
 import math
 
 
-def cria_matriz_quadrada(tamanho):
-    ponto_medio = math.ceil(tamanho/2)
-    print(ponto_medio)
+def cria_matriz_quadrada(tamanho=20):
     matriz = []
-    for i in range(tamanho):
-        linha = []
-        for j in range(tamanho):
-            if i == j and j < ponto_medio:
-                linha.append('B')
-            elif i == j and j >= ponto_medio:
-                linha.append('A')
-            elif i == tamanho - 1 - j < ponto_medio:
-                linha.append('Y')
-            elif i == tamanho - 1 - j >= ponto_medio:
-                linha.append('X')
-            else:
-                linha.append('0')
-        matriz.append(linha)
+    # FIXME ajustar para 20 apenas
+    if tamanho <= 20:
+        for _ in range(tamanho):
+            linha = ['0' for _ in range(tamanho)]
+            matriz.append(linha)
+    else:
+        raise ValueError('Tamanho deve ser no máximo 20')
     return matriz
 
 
+def diagonais(matriz):
+    tamanho = len(matriz)
+    diagonal_principal: list = []
+    diagonal_secundaria: list = []
+    top, bottom, right, left = 'B', 'A', 'Y', 'X'
+    # FIXME ajustar para 20 apenas
+    if tamanho <= 20:
+        ponto_medio = math.ceil(tamanho / 2)
+        diagonal_principal = [j for j in range(tamanho)]
+        diagonal_secundaria = [j for j in range(tamanho)[::-1]]
+
+        for i, j in enumerate(diagonal_secundaria):
+            matriz[i][j] = right if (i < ponto_medio) else left
+        for i, j in enumerate(diagonal_principal):
+            matriz[i][j] = top if (j < ponto_medio) else bottom
+    return diagonal_principal, diagonal_secundaria
+
+
+def quadrantes(matriz, diagonal_p, diagonal_s):
+    tamanho = len(matriz)
+    # FIXME ajustar para 20 apenas
+    if tamanho <= 20:
+        for i in range(tamanho):
+            elemento_dp = diagonal_p[i]
+            elemento_ds = diagonal_s[i]
+            for j in range(tamanho):
+                if elemento_dp < j < elemento_ds:
+                    matriz[i][j] = 'B'
+                elif elemento_ds < j < elemento_dp:
+                    matriz[i][j] = 'A'
+                elif j < elemento_dp and j < elemento_ds:
+                    matriz[i][j] = 'X'
+                elif j > elemento_dp and j > elemento_ds:
+                    matriz[i][j] = 'Y'
+
+
 def imprime_matriz(matriz):
-    print(f'matriz de tamanho: {len(matriz)}')
-    for linha in matriz:
-        print('\t'.join(linha))
-    print('\n')
+    try:
+        print(f'matriz de tamanho: {len(matriz)}')
+        for linha in matriz:
+            print('\t'.join(linha))
+        print('\n')
+    except ValueError as e:
+        print(e)
 
 
-tamanho = 6
-matriz = cria_matriz_quadrada(tamanho)
-imprime_matriz(matriz)
+tam = 6
+mat = cria_matriz_quadrada(tam)
+dp, ds = diagonais(mat)
+quadrantes(mat, dp, ds)
+imprime_matriz(mat)
 
-tamanho = 5
-matriz = cria_matriz_quadrada(tamanho)
-imprime_matriz(matriz)
+tam = 5
+mat = cria_matriz_quadrada(tam)
+dp, ds = diagonais(mat)
+quadrantes(mat, dp, ds)
+imprime_matriz(mat)
